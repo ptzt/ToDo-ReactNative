fc256cdf72ae269411b6a39e73c01b3b70296313
Tomas Millan <ignaciotomasrodriguezmillan@gmail.com>
2023-08-09 15:23:17 -0300

    Local Storage working fine with tasks

--- App.js
+++ App.js
@@ -1,39 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Modal, Alert, TextInput, FlatList } from 'react-native';
-import { useState, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Alert, TextInput, FlatList, Vibration } from 'react-native';
+import { useState, useRef, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Task from './components/Task';
 
 export default function App() {
 
   const [modalVisible, setModalVisible] = useState(false)
-  const [tasks, setTasks] = useState('')
   const [newTask, setNewTask] = useState('')
   const inputRef = useRef(null)
   const [temporalTask, setTemporalTask] = useState([]);
   const [dailyTask, setDailyTask] = useState([]);
 
-  const addTask = (type) => {
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const temporalTasks = await AsyncStorage.getItem('temporalTasks')
+        const dailyTasks = await AsyncStorage.getItem('dailyTasks')
+
+        if (temporalTasks) {
+          setTemporalTask(JSON.parse(temporalTasks))
+        }
+
+        if (dailyTasks) {
+          setDailyTask(JSON.parse(dailyTasks))
+        }
+
+      } catch (error) {
+        console.error('Error al cargar las tareas:', error)
+      }
+    }
+    fetchTasks()
+  }, [])
+
+
+
+  const addTask = async (type) => {
     if (newTask !== '') {
       const newTaskObj = { text: newTask, completed: false, type: type }
-      if (type === 'temporal') {
-        setTemporalTask([...temporalTask, newTaskObj])
-      } else if (type === 'diaria') {
-        setDailyTask([...dailyTask, newTaskObj])
-      }
 
-      setNewTask('')
-      setModalVisible(false)
-      Alert.alert('Se agrego!')
+      try {
+        if (type === 'temporal') {
+          setTemporalTask([...temporalTask, newTaskObj])
+          await AsyncStorage.setItem('temporalTasks', JSON.stringify([...temporalTask, newTaskObj]))
+        } else if (type === 'diaria') {
+          setDailyTask([...dailyTask, newTaskObj])
+          await AsyncStorage.setItem('dailyTasks', JSON.stringify([...temporalTask, newTaskObj]))
+        }
+        setNewTask('')
+        setModalVisible(false)
+        Alert.alert('Se agrego!')
+      } catch (error) {
+        console.error('Error al agregar la tarea:', error)
+      }
     } else {
       Alert.alert('La tarea no puede estar vacia!')
     }
+    Vibration.vibrate(150)
   }
 
-  const toggleTask = (index, type) => {
+  const toggleTask = async (index, type) => {
     if (type === 'temporal') {
-      const newTask = temporalTask.filter((_, i) => i !== index)
-      setTemporalTask(newTask)
+      const newTempTask = temporalTask.filter((_, i) => i !== index)
+      setTemporalTask(newTempTask)
+
+      try {
+        await AsyncStorage.setItem('temporalTasks', JSON.stringify(newTempTask))
+      } catch (error) {
+        console.error('Error al actualizar las tareas temporales:', error)
+      }
+
     } else if (type === 'diaria') {
       const newDailyTask = dailyTask.map((task, i) => {
         if (i === index) {
@@ -45,12 +82,19 @@ export default function App() {
         return task
       })
       setDailyTask(newDailyTask)
+
+      try {
+        await AsyncStorage.setItem('dailyTasks', JSON.stringify(newDailyTask))
+      } catch (error) {
+        console.error('Error al actualizar las tareas diarias:', error)
+      }
     }
-
-
-
   }
 
+  handleCloseModal = () => {
+    setModalVisible(false)
+    Vibration.vibrate(150)
+  }
 
 
   return (
@@ -72,12 +116,12 @@ export default function App() {
               <TouchableOpacity style={styles.button} onPress={() => { addTask('temporal') }}><Text>Tarea temporal</Text></TouchableOpacity>
               <TouchableOpacity style={styles.button} onPress={() => { addTask('diaria') }}><Text>Tarea diaria</Text></TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.button} onPress={() => setModalVisible(false)}><Text>Cancelar</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleCloseModal}><Text>Cancelar</Text></TouchableOpacity>
           </View>
         </View>
       </Modal>
       <View style={styles.header}>
-        <Text style={styles.leftText}>ToDo</Text>
+        <Text style={styles.leftText}>To-Do</Text>
         <TouchableOpacity style={styles.buttonRigth} onPress={() => {
           setModalVisible(true);
           setTimeout(() => {
